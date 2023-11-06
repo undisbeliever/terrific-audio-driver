13e7b0ee2745458105955221f7cf7c6c3d177cd3
Marcus Rowe <undisbeliever@gmail.com>
2023-11-06 21:07:15 +1000

    Fix shvc-sound-emu build script when compiling in windows

--- crates/shvc-sound-emu/build.rs
+++ crates/shvc-sound-emu/build.rs
@@ -1,8 +1,9 @@
 fn main() {
     cxx_build::bridge("src/lib.rs")
         .file("src/shvc-sound-emu.cpp")
+        .cpp(true)
+        .std("c++17")
         .include("src")
-        .flag_if_supported("-std=c++17")
         .warnings(false)
         .compile("cxx-apu");
 
