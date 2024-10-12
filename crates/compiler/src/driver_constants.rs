d4941803c86a218f4f6a656458cad2b1ec623d87
Marcus Rowe <undisbeliever@gmail.com>
2024-10-12 10:22:25 +1000

    Increase audio driver code size by 512 bytes
    I am going to add a lot of new instructions to the next release
    of the audio driver.

--- crates/compiler/src/driver_constants.rs
+++ crates/compiler/src/driver_constants.rs
@@ -60,7 +60,7 @@ pub mod addresses {
     );
 
     // MUST match `audio-driver/src/common_memmap.wiz`
-    pub const COMMON_DATA: u16 = 0xA00 - 24;
+    pub const COMMON_DATA: u16 = 0xC00 - 24;
 
     const _: () = assert!(
         _symbols::_LAST_LOADER_SYMBOL < DRIVER_CODE,
