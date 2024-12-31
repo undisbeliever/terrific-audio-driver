undisbeliever/terrific-audio-driver

commit d1604d651f16f4dd58da3e5f15b6ab80c87ea453

Process first song tick immediately
So the echo registers are setup before the first sfx tick

audio-driver/src/audio-driver.wiz
@@ -443,6 +443,7 @@ inline func mul(u8a : u8 in a, u8b : u8 in y) : u16 in ya {
 in code {
 
 // MUST be the first thing in the `code` bank (so the starting address is consistent across builds).
+#[fallthrough]
 func main() {
     sp = x = 0xff;
     direct_page = false;
@@ -661,7 +662,11 @@ func main() {
 
     io_commands.init__inline();
 
+// Fallthrough into mainloop()
+}
 
+
+func mainloop() {
     // Reset counters to ensure `maxTimerCounter` is accurate
     // Timers and counters reset when the timer control bits transition from 0 to 1
     smp.control = 0;
@@ -672,31 +677,37 @@ func main() {
         smp.control = smp.CONTROL__ENABLE_TIMER_0 | smp.CONTROL__ENABLE_TIMER_1;
     }
 
-    ^while true {
+    // Process first song tick immediately
+    // So echo registers are setup before the first sfx tick
+    goto ProcessMusicChannel;
+
+    Loop:
         io_commands.process_io_ports__inline();
 
         // Song tick timer
         a = smp.counter_0;
-        if !zero {
+        goto SkipMusicTick if zero;
             if a >= maxTimerCounter {
                 maxTimerCounter = a;
             }
 
+        ProcessMusicChannel:
             songTickCounter++;
-
             _process_music_channels__inline();
-        }
+    SkipMusicTick:
 
         // SFX tick timer
         a = smp.counter_1;
-        if !zero {
+        goto SkipSfxTick if zero;
             if a >= maxTimerCounter {
                 maxTimerCounter = a;
             }
 
             _process_sfx_channels__inline();
-        }
-    }
+    SkipSfxTick:
+
+        // Loop forever
+        ^goto Loop;
 }
 
 

commit 58fc57a60a6e5b5d68491784d4ab97ed581b9497

Fix audio driver not paused in create_and_process_song_interpreter()
This bug exhibits as single music channel's playing from the beginning
with incorrect channel settings (instrument, echo, etc).

The bug was caused in commit d1604d651f16f4dd58da3e5f15b6ab80c87ea453
(Process first song tick immediately), which added an unknown delay to
the songs initialisation time (depending on the number of first tick
instructions).  Causing `InterpreterOutput::write_to_emulator()` to
write to audio-RAM while the spc700 code was also processing a music
channel.

This commit fixes the bug by:
 1. Changing `process_music_channels()` so it is not inline.
 2. Adding a `is_pc_in_mainloop()` test to the emulator.
 3. Adding a panic to `InterpreterOutput::write_to_emulator()` if the
    audio driver is not in `mainloop()`.
 4. Use `is_pc_in_mainloop()` to detect the end of audio-driver
    initialisation in `_load_song_into_memory()`.

audio-driver/src/audio-driver.wiz
@@ -689,14 +689,13 @@ func mainloop() {
 
         // Song tick timer
         a = smp.counter_0;
-        ^goto SkipMusicTick if zero;
+        goto SkipMusicTick if zero;
             if a >= maxTimerCounter {
                 maxTimerCounter = a;
             }
 
         ProcessMusicChannel:
-            songTickCounter++;
-            _process_music_channels__inline();
+            process_music_channels();
     SkipMusicTick:
 
         // SFX tick timer
@@ -753,7 +752,14 @@ inline func __clear_echo_buffer__inline(esa : u8 in x) {
 }
 
 
-inline func _process_music_channels__inline() {
+// Process music channels.
+//
+// Not inline for 2 reasons:
+//   1. Used by the GUI to determine if the audio driver is processing music channels.
+//   2. Will be required when I add loading ditties to the audio driver.
+func process_music_channels() {
+    songTickCounter++;
+
     y = a = keyOffShadow_music & musicSfxChannelMask;
     write_dsp(GlobalDspAddr.KOFF, y);
 

crates/compiler/build.rs
@@ -28,6 +28,8 @@ const TAD_IO_VERSION: &str = "TAD_IO_VERSION";
 #[rustfmt::skip]
 const AUDIO_DRIVER_SYMBOLS: &[(&str, &str)] = &[
     ("main", "DRIVER_CODE"),
+    ("mainloop", "MAINLOOP_CODE"),
+    ("process_music_channels", "PROCESS_MUSIC_CHANNELS_CODE"),
     ("__loader_songPtr", "SONG_PTR"),
     ("__loader_dataType", "LOADER_DATA_TYPE"),
     ("songTickCounter", "SONG_TICK_COUNTER"),

crates/compiler/src/bytecode_interpreter.rs
@@ -1774,6 +1774,12 @@ pub trait Emulator {
     fn apuram_mut(&mut self) -> &mut [u8; 0x10000];
     fn write_dsp_register(&mut self, addr: u8, value: u8);
     fn write_smp_register(&mut self, addr: u8, value: u8);
+    fn program_counter(&self) -> u16;
+
+    // CAUTION: might return true when program_counter() at the start or end of _process_sfx_channels__inline().
+    fn is_pc_in_mainloop(&self) -> bool {
+        addresses::MAIN_LOOP_CODE_RANGE.contains(&self.program_counter())
+    }
 }
 
 /// Writes `InterpreterOutput` to the emulator.
@@ -1786,6 +1792,16 @@ pub trait Emulator {
 /// SAFETY: panics if the audio driver is not the paused state
 impl InterpreterOutput {
     fn write_to_emulator(&self, emu: &mut impl Emulator) {
+        // MUST NOT modify sound effect channels.
+        //
+        // (pc_in_mainloop() can return true if in `_process_sfx_channels__inline()`)
+
+        if !emu.is_pc_in_mainloop() {
+            panic!(
+                "audio driver is not paused (it might processing music channels or an IO command)"
+            );
+        }
+
         let key_on_shadow: u8 = self
             .channels
             .iter()
@@ -1819,8 +1835,6 @@ impl InterpreterOutput {
                 self.song_data_addr,
                 "songPtr does not match"
             );
-
-            // ::TODO find a way to determine if the audio driver is paused::
 
             let mut apu_write = |addr: u16, value: u8| {
                 apuram[usize::from(addr)] = value;

crates/compiler/src/driver_constants.rs
@@ -15,6 +15,8 @@ mod _symbols {
 }
 
 pub mod addresses {
+    use std::ops::Range;
+
     use super::_symbols;
 
     macro_rules! declare_symbols {
@@ -27,6 +29,8 @@ pub mod addresses {
 
     declare_symbols!(
         DRIVER_CODE,
+        MAINLOOP_CODE,
+        PROCESS_MUSIC_CHANNELS_CODE,
         LOADER,
         SONG_PTR,
         LOADER_DATA_TYPE,
@@ -109,6 +113,12 @@ pub mod addresses {
         (COMMON_DATA as usize + super::COMMON_DATA_HEADER_SIZE) & 0xff == 0,
         "BRR directory is not page aligned"
     );
+
+    const _: () = assert!(
+        MAINLOOP_CODE < PROCESS_MUSIC_CHANNELS_CODE,
+        "mainloop() must be before process_music_channels()"
+    );
+    pub const MAIN_LOOP_CODE_RANGE: Range<u16> = MAINLOOP_CODE..PROCESS_MUSIC_CHANNELS_CODE;
 }
 
 pub const ECHO_VARIABLES_SIZE: usize = (addresses::ECHO_DIRTY - addresses::ECHO_VARIABLES) as usize;

crates/shvc-sound-emu/src/lib.rs
@@ -40,6 +40,8 @@ mod ffi {
             sp: u8,
         );
 
+        fn program_counter(self: &ShvcSoundEmu) -> u16;
+
         fn emulate(self: Pin<&mut ShvcSoundEmu>) -> &[i16; 512];
     }
 }
@@ -107,6 +109,10 @@ impl ShvcSoundEmu {
 
     pub fn set_spc_registers(&mut self, pc: u16, a: u8, x: u8, y: u8, psw: u8, sp: u8) {
         self.emu.pin_mut().set_spc_registers(pc, a, x, y, psw, sp)
+    }
+
+    pub fn program_counter(self: &ShvcSoundEmu) -> u16 {
+        self.emu.program_counter()
     }
 
     pub fn emulate(&mut self) -> &[i16; Self::AUDIO_BUFFER_SIZE] {

crates/shvc-sound-emu/src/shvc-sound-emu.cpp
@@ -90,6 +90,10 @@ auto ShvcSoundEmu::set_spc_registers(uint16_t pc, uint8_t a, uint8_t x, uint8_t 
   smp.r.s = sp;
 }
 
+auto ShvcSoundEmu::program_counter() const -> uint16_t {
+  return smp.r.pc.w;
+}
+
 auto ShvcSoundEmu::emulate() -> const std::array<int16_t, AUDIO_BUFFER_SIZE>& {
   smp.dsp.sampleBuffer.reset();
 

crates/shvc-sound-emu/src/shvc-sound-emu.hpp
@@ -50,6 +50,8 @@ struct ShvcSoundEmu {
 
   auto set_spc_registers(uint16_t pc, uint8_t a, uint8_t x, uint8_t y, uint8_t psw, uint8_t sp) -> void;
 
+  auto program_counter() const -> uint16_t;
+
   auto emulate() -> const std::array<int16_t, AUDIO_BUFFER_SIZE>&;
 
 private:

crates/tad-gui/src/audio_thread.rs
@@ -9,6 +9,7 @@
 use brr::{BrrSample, SAMPLES_PER_BLOCK};
 use compiler::audio_driver;
 use compiler::bytecode_interpreter;
+use compiler::bytecode_interpreter::Emulator;
 use compiler::bytecode_interpreter::SongInterpreter;
 use compiler::common_audio_data::CommonAudioData;
 use compiler::driver_constants::N_CHANNELS;
@@ -410,6 +411,10 @@ impl bytecode_interpreter::Emulator for EmulatorWrapper<'_> {
     fn write_smp_register(&mut self, addr: u8, value: u8) {
         self.0.write_smp_register(addr, value);
     }
+
+    fn program_counter(&self) -> u16 {
+        self.0.program_counter()
+    }
 }
 
 enum AudioDataState {
@@ -437,7 +442,7 @@ impl std::ops::Deref for SiCad {
 }
 
 fn create_and_process_song_interpreter(
-    emu: &mut ShvcSoundEmu,
+    emu: &mut EmulatorWrapper,
     audio_data: &AudioDataState,
     song_skip: SongSkip,
     stereo_flag: bool,
@@ -456,7 +461,7 @@ fn create_and_process_song_interpreter(
         SongSkip::None => Ok(Some(SongInterpreter::new(cad, sd, stereo_flag))),
         SongSkip::Song(ticks) => {
             let mut si = SongInterpreter::new(cad, sd, stereo_flag);
-            if si.process_song_skip_ticks(ticks, &mut EmulatorWrapper(emu)) {
+            if si.process_song_skip_ticks(ticks, emu) {
                 Ok(Some(si))
             } else {
                 Ok(None)
@@ -465,7 +470,7 @@ fn create_and_process_song_interpreter(
         SongSkip::Subroutine(prefix, si, ticks) => {
             match SongInterpreter::new_song_subroutine(cad, sd, prefix, si, stereo_flag) {
                 Ok(mut si) => {
-                    if si.process_song_skip_ticks(ticks, &mut EmulatorWrapper(emu)) {
+                    if si.process_song_skip_ticks(ticks, emu) {
                         Ok(Some(si))
                     } else {
                         Ok(None)
@@ -718,10 +723,13 @@ impl TadEmu {
             .set_spc_registers(addresses::DRIVER_CODE, 0, 0, 0, 0, 0xff);
 
         // Wait for the audio-driver to finish initialization
-        self.emu.emulate();
+        let mut emu_wrapper = EmulatorWrapper(&mut self.emu);
+        while !emu_wrapper.is_pc_in_mainloop() {
+            emu_wrapper.0.emulate();
+        }
 
         self.bc_interpreter = create_and_process_song_interpreter(
-            &mut self.emu,
+            &mut emu_wrapper,
             &data_state,
             song_skip,
             stereo_flag,

crates/compiler/examples/test_bc_interpreter.rs
@@ -95,6 +95,11 @@ impl bytecode_interpreter::Emulator for DummyEmu {
     fn write_smp_register(&mut self, addr: u8, value: u8) {
         self.apuram[usize::from(addr)] = value;
     }
+
+    // HACK: Always return a mainloop() address
+    fn program_counter(&self) -> u16 {
+        addresses::MAINLOOP_CODE
+    }
 }
 
 fn mask_channel_soa(