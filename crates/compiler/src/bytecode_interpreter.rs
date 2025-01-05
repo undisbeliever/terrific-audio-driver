undisbeliever/terrific-audio-driver

commit 97395919131511ea7251bf696fa47320aaa31624

Add set_echo_delay instruction

crates/compiler/src/bytecode_interpreter.rs
@@ -1138,6 +1138,11 @@ impl ChannelState {
             opcodes::SET_ECHO_INVERT => {
                 global.echo.invert_flags = read_pc();
             }
+            opcodes::SET_ECHO_DELAY => {
+                let edl = read_pc();
+
+                global.echo.edl = min(edl, global.echo.max_edl);
+            }
 
             opcodes::DISABLE_CHANNEL => self.disable_channel(),
 
@@ -1252,6 +1257,7 @@ impl ChannelState {
             opcodes::ADJUST_ECHO_I8 => Some(3),
             opcodes::ADJUST_ECHO_I8_LIMIT => Some(4),
             opcodes::SET_ECHO_INVERT => Some(2),
+            opcodes::SET_ECHO_DELAY => Some(2),
 
             opcodes::DISABLE_CHANNEL => None,
 
@@ -1831,6 +1837,7 @@ fn unused_channel(channel_index: usize) -> Channel {
 
 pub trait Emulator {
     fn apuram_mut(&mut self) -> &mut [u8; 0x10000];
+    fn set_echo_buffer_size(&mut self, esa: u8, edl: u8);
     fn write_dsp_register(&mut self, addr: u8, value: u8);
     fn write_smp_register(&mut self, addr: u8, value: u8);
     fn program_counter(&self) -> u16;
@@ -2050,6 +2057,10 @@ impl InterpreterOutput {
             // The audio driver's virtual channels will write to the DSP for me.
 
             emu.write_dsp_register(S_DSP_EON_REGISTER, eon_shadow);
+
+            let echo_size = EchoEdl::try_from(self.echo.max_edl).unwrap().buffer_size();
+            let esa = (0xffff - echo_size + 1).to_le_bytes()[1];
+            emu.set_echo_buffer_size(esa, self.echo.edl);
         }
 
         emu.write_smp_register(S_SMP_TIMER_0_REGISTER, self.tick_clock);

crates/compiler/src/songs.rs
@@ -17,6 +17,7 @@ use crate::driver_constants::{
     MAX_SUBROUTINES, N_MUSIC_CHANNELS, SFX_TICK_CLOCK, SONG_HEADER_CHANNELS_SIZE,
     SONG_HEADER_N_SUBROUTINES_OFFSET, SONG_HEADER_SIZE, SONG_HEADER_TICK_TIMER_OFFSET,
 };
+use crate::echo::EchoEdl;
 use crate::envelope::{Envelope, Gain};
 use crate::errors::{ChannelError, SongError, SongTooLargeError};
 use crate::mml::{MetaData, Section};
@@ -215,7 +216,10 @@ pub fn test_sample_song(
     envelope: Option<Envelope>,
 ) -> Result<SongData, ChannelError> {
     let mut bc = Bytecode::new(
-        BytecodeContext::SongChannel { index: 0 },
+        BytecodeContext::SongChannel {
+            index: 0,
+            max_edl: EchoEdl::MIN,
+        },
         sample_song_fake_instruments(),
         None,
     );

crates/tad-gui/src/audio_thread.rs
@@ -404,6 +404,10 @@ impl bytecode_interpreter::Emulator for EmulatorWrapper<'_> {
         self.0.apuram_mut()
     }
 
+    fn set_echo_buffer_size(&mut self, esa: u8, edl: u8) {
+        self.0.set_echo_buffer_size(esa, edl);
+    }
+
     fn write_dsp_register(&mut self, addr: u8, value: u8) {
         self.0.write_dsp_register(addr, value);
     }

crates/compiler/examples/test_bc_interpreter.rs
@@ -20,6 +20,7 @@ use compiler::{
     samples::build_sample_and_instrument_data,
     songs::SongData,
     sound_effects::blank_compiled_sound_effects,
+    spc_file_export::{S_DSP_EDL_REGISTER, S_DSP_ESA_REGISTER},
     time::TickCounter,
 };
 use shvc_sound_emu::ShvcSoundEmu;
@@ -88,6 +89,11 @@ impl bytecode_interpreter::Emulator for DummyEmu {
         &mut self.apuram
     }
 
+    fn set_echo_buffer_size(&mut self, esa: u8, edl: u8) {
+        self.write_dsp_register(S_DSP_ESA_REGISTER as u8, esa);
+        self.write_dsp_register(S_DSP_EDL_REGISTER as u8, edl);
+    }
+
     fn write_dsp_register(&mut self, addr: u8, value: u8) {
         self.dsp_registers[usize::from(addr)] = value;
     }
@@ -134,6 +140,11 @@ fn assert_bc_intrepreter_matches_emu(
         intrepreter_memory.dsp_registers[usize::from(S_DSP_EON_REGISTER)],
         emu.dsp_registers()[usize::from(S_DSP_EON_REGISTER)],
         "EON S-DSP register mismatch (tick_count: {tick_count})"
+    );
+    assert_eq!(
+        intrepreter_memory.dsp_registers[S_DSP_ESA_REGISTER],
+        emu.dsp_registers()[S_DSP_ESA_REGISTER],
+        "ESA S-DSP register mismatch (tick_count: {tick_count})"
     );
 
     let int_apuram = &intrepreter_memory.apuram;

audio-driver/src/audio-driver.wiz
@@ -299,7 +299,11 @@ in zeropage {
         let ECHO_DIRTY__FIR_FILTER_BIT = 7;
         let ECHO_DIRTY__CLEAR_FIR_BIT = 0;  // Clear FIR filter before writing FIR filter
         let ECHO_DIRTY__VOLUME_BIT = 6;
+
+        // Not tested in `_process_echo_registers__inline()`
+        // Instead they will be written whenever echoDirty is non-zero
         let ECHO_DIRTY__FEEDBACK_BIT = 5;
+        let ECHO_DIRTY__EDL_BIT = 4;
 
     // The maximum echo delay in the song.
     // Used to determine the size and location of the echo buffer in Audio-RAM.
@@ -1172,6 +1176,10 @@ inline func _process_echo_registers__inline() {
         // Always update echo feedback
         smp.dsp_addr = GlobalDspAddr.EFB as u8;
         smp.dsp_data = echo.echoFeedback as u8;
+
+        // Always update echo delay
+        smp.dsp_addr = GlobalDspAddr.EDL as u8;
+        smp.dsp_data = echo.edl;
 
         echoDirty = 0;
     }
@@ -3460,6 +3468,18 @@ func set_echo_invert(channelIndex : u8 in x, flags: u8 in a) {
     ^return process_next_bytecode(x);
 }
 
+
+// KEEP: X
+func set_echo_delay(channelIndex : u8 in x, edl: u8 in a) {
+    if a > maxEdl {
+        a = maxEdl;
+    }
+    echo.edl = a;
+
+    echoDirty $ ECHO_DIRTY__EDL_BIT = true;
+
+    ^return process_next_bytecode(x);
+}
 
 
 const InstructionTable : [u16 ; 64] = [ f as u16 for let f in INSTRUCTIONS_WITH_ARGUMENTS ]

audio-driver/src/bytecode.wiz
@@ -383,6 +383,10 @@ let INSTRUCTIONS_WITH_ARGUMENTS = [
 
     // parameter = rl00000m (same parameter as `set_channel_invert`)
     set_echo_invert,
+
+    // parameter = u8 EDL echo delay value
+    // If the parameter > maxEdl, maxEdl will be used instead
+    set_echo_delay,
 ];
 
 

crates/compiler/src/bytecode.rs
@@ -12,7 +12,7 @@ use crate::driver_constants::{
     BC_CHANNEL_STACK_SIZE, BC_STACK_BYTES_PER_LOOP, BC_STACK_BYTES_PER_SUBROUTINE_CALL,
     FIR_FILTER_SIZE, MAX_INSTRUMENTS_AND_SAMPLES,
 };
-use crate::echo::{EchoFeedback, EchoVolume, FirCoefficient, FirTap};
+use crate::echo::{EchoEdl, EchoFeedback, EchoLength, EchoVolume, FirCoefficient, FirTap};
 use crate::envelope::{Adsr, Envelope, Gain, OptionalGain, TempGain};
 use crate::errors::{BytecodeError, ChannelError, ValueError};
 use crate::invert_flags::InvertFlags;
@@ -193,8 +193,8 @@ impl RelativeFirCoefficient {
 }
 
 pub enum BytecodeContext {
-    SongSubroutine {},
-    SongChannel { index: u8 },
+    SongSubroutine { max_edl: EchoEdl },
+    SongChannel { index: u8, max_edl: EchoEdl },
     SoundEffect,
     MmlPrefix,
 }
@@ -272,6 +272,7 @@ pub mod opcodes {
         ADJUST_ECHO_I8,
         ADJUST_ECHO_I8_LIMIT,
         SET_ECHO_INVERT,
+        SET_ECHO_DELAY,
         END_LOOP,
         RETURN_FROM_SUBROUTINE_AND_DISABLE_VIBRATO,
         RETURN_FROM_SUBROUTINE,
@@ -2339,5 +2340,22 @@ impl<'a> Bytecode<'a> {
 
     pub fn set_echo_invert(&mut self, flags: InvertFlags) {
         emit_bytecode!(self, opcodes::SET_ECHO_INVERT, flags.into_driver_value())
+    }
+
+    pub fn set_echo_delay(&mut self, length: EchoLength) -> Result<(), BytecodeError> {
+        match self.context {
+            BytecodeContext::SongSubroutine { max_edl }
+            | BytecodeContext::SongChannel { max_edl, .. } => {
+                let edl = length.to_edl();
+                if edl.as_u8() <= max_edl.as_u8() {
+                    emit_bytecode!(self, opcodes::SET_ECHO_DELAY, edl.as_u8());
+                    Ok(())
+                } else {
+                    Err(BytecodeError::EchoLengthLargerThanMaxEdl { edl, max_edl })
+                }
+            }
+            BytecodeContext::SoundEffect => Err(BytecodeError::CannotSetEchoDelayInSoundEffect),
+            BytecodeContext::MmlPrefix => Err(BytecodeError::CannotSetEchoDelayInMmlPrefix),
+        }
     }
 }

crates/compiler/src/bytecode_assembler.rs
@@ -607,6 +607,7 @@ pub fn parse_asm_line(bc: &mut Bytecode, line: &str) -> Result<(), ChannelError>
        adjust_fir_tap 2 uvnt_and_svnt_arguments,
        adjust_fir_tap_limit 3 adjust_fir_tap_limit_arguments,
        set_echo_invert 1 parse_invert_flag_arguments,
+       set_echo_delay 1 one_uvnt_argument,
     )
 }
 

commit 939a0d4064b87bfb322cfaf134d624040dc9c5d5

Fix weird sounding music in the GUI
This bug was introduced in commit 97395919131511ea7251bf696fa47320aaa31624
(Add set_echo_delay instruction).

The GUI calls `ShvcSoundEmu::set_echo_buffer_size()` after ShvcSoundEmu
has emulated the audio driver initialisation code.  Removing the
`set_echo_buffer_size()` call fixes the bug but prevents
play-from-cursor from handling `set_echo_delay` instructions.

This commit fixes the bug by using the rust bytecode interpreter to
get the correct ESA/EDL register values and patching the song header's
echo.edl value before emulating audio driver code.

audio-driver/src/data-formats.wiz
@@ -147,6 +147,8 @@ struct EchoBufferSettings {
     //       The echo buffer may clobber song/sample/sfx data if `edl` is too large.
     //
     // CAUTION: This field does not contain the maximum EDL when in zeropage.
+    //
+    // MUST be first
     edl : u8,
 
     // FIR filter.  Written to the `C0`-`C7` S-DSP registers.

crates/compiler/src/bytecode_interpreter.rs
@@ -145,6 +145,13 @@ impl EchoVariables {
 
         out
     }
+
+    fn song_header_edl(&self) -> u8 {
+        assert!(self.max_edl <= EchoEdl::MAX.as_u8());
+        assert!(self.edl <= self.max_edl);
+
+        (self.max_edl << 4) | self.edl
+    }
 }
 
 struct InterpreterOutput {
@@ -1488,25 +1495,36 @@ where
     }
 
     #[must_use]
-    pub fn process_song_skip_ticks(&mut self, ticks: TickCounter, emu: &mut impl Emulator) -> bool {
+    pub fn process_song_skip_ticks(&mut self, ticks: TickCounter) -> bool {
         assert!(self.tick_counter.is_zero());
 
         if ticks.value() > 2 {
             let ticks = TickCounter::new(ticks.value() - 1);
-            let valid = self.process_ticks(ticks);
-            if valid {
-                self.write_to_emulator(emu);
-            }
-            valid
+            self.process_ticks(ticks)
         } else {
             // ticks can be 0 when playing a subroutine
-            self.write_to_emulator(emu);
             true
         }
     }
 
     pub fn tick_counter(&self) -> TickCounter {
         self.tick_counter
+    }
+
+    pub fn song_header_edl(&self) -> u8 {
+        self.global.echo.song_header_edl()
+    }
+
+    pub fn esa_register(&self) -> u8 {
+        let echo_size = EchoEdl::try_from(self.global.echo.max_edl)
+            .unwrap()
+            .buffer_size();
+
+        (0xffff - echo_size + 1).to_le_bytes()[1]
+    }
+
+    pub fn edl_register(&self) -> u8 {
+        self.global.echo.edl
     }
 
     pub fn write_to_emulator(&self, emu: &mut impl Emulator) {
@@ -1859,7 +1877,6 @@ fn unused_channel(channel_index: usize) -> Channel {
 
 pub trait Emulator {
     fn apuram_mut(&mut self) -> &mut [u8; 0x10000];
-    fn set_echo_buffer_size(&mut self, esa: u8, edl: u8);
     fn write_dsp_register(&mut self, addr: u8, value: u8);
     fn write_smp_register(&mut self, addr: u8, value: u8);
     fn program_counter(&self) -> u16;
@@ -2079,10 +2096,6 @@ impl InterpreterOutput {
             // The audio driver's virtual channels will write to the DSP for me.
 
             emu.write_dsp_register(S_DSP_EON_REGISTER, eon_shadow);
-
-            let echo_size = EchoEdl::try_from(self.echo.max_edl).unwrap().buffer_size();
-            let esa = (0xffff - echo_size + 1).to_le_bytes()[1];
-            emu.set_echo_buffer_size(esa, self.echo.edl);
         }
 
         emu.write_smp_register(S_SMP_TIMER_0_REGISTER, self.tick_clock);

crates/compiler/src/driver_constants.rs
@@ -224,6 +224,7 @@ pub const MAX_SUBROUTINES: usize = 255;
 
 pub const SONG_HEADER_SIZE: usize = N_MUSIC_CHANNELS * 2 + 15;
 pub const SONG_HEADER_CHANNELS_SIZE: usize = N_MUSIC_CHANNELS * 2;
+pub const SONG_HEADER_ECHO_EDL: usize = SONG_HEADER_CHANNELS_SIZE;
 pub const SONG_HEADER_TICK_TIMER_OFFSET: usize = SONG_HEADER_SIZE - 2;
 pub const SONG_HEADER_N_SUBROUTINES_OFFSET: usize = SONG_HEADER_SIZE - 1;
 

crates/compiler/src/songs.rs
@@ -15,7 +15,8 @@ use crate::data::{self, single_item_unique_names_list, InstrumentOrSample, Name,
 use crate::driver_constants::{
     addresses, AUDIO_RAM_SIZE, ECHO_BUFFER_MIN_SIZE, ECHO_VARIABLES_SIZE, MAX_SONG_DATA_SIZE,
     MAX_SUBROUTINES, N_MUSIC_CHANNELS, SFX_TICK_CLOCK, SONG_HEADER_CHANNELS_SIZE,
-    SONG_HEADER_N_SUBROUTINES_OFFSET, SONG_HEADER_SIZE, SONG_HEADER_TICK_TIMER_OFFSET,
+    SONG_HEADER_ECHO_EDL, SONG_HEADER_N_SUBROUTINES_OFFSET, SONG_HEADER_SIZE,
+    SONG_HEADER_TICK_TIMER_OFFSET,
 };
 use crate::echo::EchoEdl;
 use crate::envelope::{Envelope, Gain};
@@ -352,6 +353,7 @@ fn write_song_header(
     const EBS: usize = SONG_HEADER_CHANNELS_SIZE;
     let echo_buffer = &metadata.echo_buffer;
 
+    const _: () = assert!(EBS == SONG_HEADER_ECHO_EDL);
     header[EBS] = (echo_buffer.max_edl.as_u8() << 4) | echo_buffer.edl.as_u8();
     for (i, f) in echo_buffer.fir.iter().enumerate() {
         header[EBS + 1 + i] = f.as_i8().to_le_bytes()[0];

crates/tad-gui/src/audio_thread.rs
@@ -15,6 +15,7 @@ use compiler::common_audio_data::CommonAudioData;
 use compiler::driver_constants::N_CHANNELS;
 use compiler::driver_constants::N_DSP_VOICES;
 use compiler::driver_constants::N_MUSIC_CHANNELS;
+use compiler::driver_constants::SONG_HEADER_ECHO_EDL;
 use compiler::driver_constants::{
     addresses, io_commands, LoaderDataType, FIRST_SFX_CHANNEL, IO_COMMAND_I_MASK, IO_COMMAND_MASK,
     N_SFX_CHANNELS,
@@ -404,10 +405,6 @@ impl bytecode_interpreter::Emulator for EmulatorWrapper<'_> {
         self.0.apuram_mut()
     }
 
-    fn set_echo_buffer_size(&mut self, esa: u8, edl: u8) {
-        self.0.set_echo_buffer_size(esa, edl);
-    }
-
     fn write_dsp_register(&mut self, addr: u8, value: u8) {
         self.0.write_dsp_register(addr, value);
     }
@@ -446,7 +443,6 @@ impl std::ops::Deref for SiCad {
 }
 
 fn create_and_process_song_interpreter(
-    emu: &mut EmulatorWrapper,
     audio_data: &AudioDataState,
     song_skip: SongSkip,
     stereo_flag: bool,
@@ -465,7 +461,7 @@ fn create_and_process_song_interpreter(
         SongSkip::None => Ok(Some(SongInterpreter::new(cad, sd, stereo_flag))),
         SongSkip::Song(ticks) => {
             let mut si = SongInterpreter::new(cad, sd, stereo_flag);
-            if si.process_song_skip_ticks(ticks, emu) {
+            if si.process_song_skip_ticks(ticks) {
                 Ok(Some(si))
             } else {
                 Ok(None)
@@ -474,7 +470,7 @@ fn create_and_process_song_interpreter(
         SongSkip::Subroutine(prefix, si, ticks) => {
             match SongInterpreter::new_song_subroutine(cad, sd, prefix, si, stereo_flag) {
                 Ok(mut si) => {
-                    if si.process_song_skip_ticks(ticks, emu) {
+                    if si.process_song_skip_ticks(ticks) {
                         Ok(Some(si))
                     } else {
                         Ok(None)
@@ -679,8 +675,6 @@ impl TadEmu {
             AudioDataState::SongWithSfxBuffer(cad, sd) => (&cad.0, sd.as_ref()),
         };
 
-        self.emu.power(true);
-
         let song_data = song.data();
         let common_data = common_audio_data.data();
         let echo_buffer = &song.metadata().echo_buffer;
@@ -689,6 +683,11 @@ impl TadEmu {
             StereoFlag::Stereo => true,
             StereoFlag::Mono => false,
         };
+
+        self.bc_interpreter =
+            create_and_process_song_interpreter(&data_state, song_skip, stereo_flag)?;
+
+        self.emu.power(true);
 
         let song_data_addr = common_audio_data.song_data_addr();
 
@@ -720,8 +719,20 @@ impl TadEmu {
         }
         .driver_value();
 
-        self.emu
-            .set_echo_buffer_size(echo_buffer.esa_register(), echo_buffer.edl_register());
+        // The echo buffer registers must be setup BEFORE the emulator processes instructions.
+        // Otherwise the audio sounds weird.
+        match &self.bc_interpreter {
+            Some(bci) => {
+                apuram[usize::from(song_data_addr) + SONG_HEADER_ECHO_EDL] = bci.song_header_edl();
+
+                self.emu
+                    .set_echo_buffer_size(bci.esa_register(), bci.edl_register());
+            }
+            None => {
+                self.emu
+                    .set_echo_buffer_size(echo_buffer.esa_register(), echo_buffer.edl_register());
+            }
+        }
 
         self.emu
             .set_spc_registers(addresses::DRIVER_CODE, 0, 0, 0, 0, 0xff);
@@ -732,12 +743,9 @@ impl TadEmu {
             emu_wrapper.0.emulate();
         }
 
-        self.bc_interpreter = create_and_process_song_interpreter(
-            &mut emu_wrapper,
-            &data_state,
-            song_skip,
-            stereo_flag,
-        )?;
+        if let Some(bci) = &self.bc_interpreter {
+            bci.write_to_emulator(&mut emu_wrapper);
+        }
 
         self.set_music_channels_mask(music_channels_mask);
 

crates/compiler/examples/test_bc_interpreter.rs
@@ -20,7 +20,6 @@ use compiler::{
     samples::build_sample_and_instrument_data,
     songs::SongData,
     sound_effects::blank_compiled_sound_effects,
-    spc_file_export::{S_DSP_EDL_REGISTER, S_DSP_ESA_REGISTER},
     time::TickCounter,
 };
 use shvc_sound_emu::ShvcSoundEmu;
@@ -89,11 +88,6 @@ impl bytecode_interpreter::Emulator for DummyEmu {
         &mut self.apuram
     }
 
-    fn set_echo_buffer_size(&mut self, esa: u8, edl: u8) {
-        self.write_dsp_register(S_DSP_ESA_REGISTER as u8, esa);
-        self.write_dsp_register(S_DSP_EDL_REGISTER as u8, edl);
-    }
-
     fn write_dsp_register(&mut self, addr: u8, value: u8) {
         self.dsp_registers[usize::from(addr)] = value;
     }
@@ -140,11 +134,6 @@ fn assert_bc_intrepreter_matches_emu(
         intrepreter_memory.dsp_registers[usize::from(S_DSP_EON_REGISTER)],
         emu.dsp_registers()[usize::from(S_DSP_EON_REGISTER)],
         "EON S-DSP register mismatch (tick_count: {tick_count})"
-    );
-    assert_eq!(
-        intrepreter_memory.dsp_registers[S_DSP_ESA_REGISTER],
-        emu.dsp_registers()[S_DSP_ESA_REGISTER],
-        "ESA S-DSP register mismatch (tick_count: {tick_count})"
     );
 
     let int_apuram = &intrepreter_memory.apuram;